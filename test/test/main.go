629a0ce631e8efe9c26574fcdca239eb68df9d58
huangweiwei <hww9035@qq.com>
2022-09-27 14:55:22 +0800

    test

--- test/test/main.go
+++ test/test/main.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"context"
+	"crypto/hmac"
+	"crypto/md5"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
 	"fmt"
+	"io"
 	"time"
 	"unsafe"
 )
@@ -38,7 +44,7 @@ func gen2(ctx context.Context) {
 	}
 }
 
-func main() {
+func testContext() {
 	ctx, cancel := context.WithCancel(context.Background())
 
 	for n := range gen(ctx) {
@@ -49,4 +55,31 @@ func main() {
 			break
 		}
 	}
+}
+
+func ComputeHmacSha256(message string, secret string) string {
+	key := []byte(secret)
+	h := hmac.New(sha256.New, key)
+	h.Write([]byte(message))
+	sha := hex.EncodeToString(h.Sum(nil))
+	//	hex.EncodeToString(h.Sum(nil))
+	return base64.StdEncoding.EncodeToString([]byte(sha))
+}
+
+func md5ByString(str string) string {
+	//方法一
+	//data := []byte(str)
+	//has := md5.Sum(data)
+	//md5str1 := fmt.Sprintf("%x", has)
+
+	//方法二
+	m := md5.New()
+	_, _ = io.WriteString(m, str)
+	arr := m.Sum(nil)
+	//md5str2 := hex.EncodeToString(arr)
+	md5str2 := fmt.Sprintf("%x", arr)
+	return md5str2
+}
+
+func main() {
 }

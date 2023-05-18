a152ba0bb1ffd232ffb9498030680a948d4e1dc1
huangweiwei <hww9035@qq.com>
2023-05-18 19:18:39 +0800

    test

--- test/test.go
+++ test/test.go
@@ -46,3 +46,58 @@ func TestNsq() {
 	}()
 	nsq.TestConsumer("127.0.0.1:4161", "top1", "chan1")
 }
+
+// Determinant
+//
+//	@Description:
+//
+// println(Determinant([][]int{{1}}))
+// println(Determinant([][]int{{1, 3}, {2, 5}}))
+// println(Determinant([][]int{{2, 5, 3}, {1, -2, -1}, {1, 3, 4}}
+//
+//	@param matrix
+//	@return int
+func Determinant(matrix [][]int) int {
+	if len(matrix) <= 1 {
+		return matrix[0][0]
+	}
+	if len(matrix) == 2 {
+		return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
+	}
+	f := 0
+	sum := 0
+	// 递归思想
+	for i := 0; i < len(matrix[0]); i++ {
+		if i == 0 {
+			sum = matrix[0][0] * Determinant(makeArr(&matrix, 0))
+			continue
+		}
+		if f == 0 {
+			//减法
+			sum = sum - matrix[0][i]*Determinant(makeArr(&matrix, i))
+			f = 1
+		} else {
+			//加法
+			sum = sum + matrix[0][i]*Determinant(makeArr(&matrix, i))
+			f = 0
+		}
+	}
+
+	return sum
+}
+
+// 构造新的切片
+func makeArr(src *[][]int, clo int) [][]int {
+	num := len(*src)
+	var data [][]int
+	for i := 1; i < num; i++ {
+		var tmp []int
+		for j := 0; j < len((*src)[i]); j++ {
+			if j != clo {
+				tmp = append(tmp, (*src)[i][j])
+			}
+		}
+		data = append(data, tmp)
+	}
+	return data
+}
